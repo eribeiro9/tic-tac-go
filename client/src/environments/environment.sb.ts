fcdfb1784c819fc613638dc3ec28660b3864084d
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2022-01-26 15:13:22 -0500

    feat: create sandbox environment

---
+++ client/src/environments/environment.sb.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  websocketURL: 'wss://sb-wss.tictacgo.xyz/v1',
+  baseUrl: 'https://sb.tictacgo.xyz',
+};
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  websocketURL: 'wss://wss.tictacgo.xyz/v1',
-  baseUrl: 'https://www.tictacgo.xyz',
+  websocketURL: 'wss://sb-wss.tictacgo.xyz/v1',
+  baseUrl: 'https://sb.tictacgo.xyz',
 };
 
 /*
